e79e1f1ffbce37581adda5943ff3f0ebebf02dd3
Wesley Mota <wesleymotabr@gmail.com>
2021-10-13 15:12:55 -0300

    +connection dentro de get_connection

--- main.py
+++ main.py
@@ -9,10 +9,22 @@ api = Api(app)
 app.config['JSON_AS_ASCII'] = False
 
 connection = pymysql.connect(host='remotemysql.com',
-                             user='riPOnUyCuK',
-                             password='m6YJ0faBdk',
-                             database='riPOnUyCuK',
-                             cursorclass=pymysql.cursors.DictCursor)
+                                    user='riPOnUyCuK',
+                                    password='m6YJ0faBdk',
+                                    database='riPOnUyCuK',
+                                    cursorclass=pymysql.cursors.DictCursor)
+
+def get_connection(connection):
+    try:
+        cur = connection.cursor()
+    except:
+        connection = pymysql.connect(host='remotemysql.com',
+                                    user='riPOnUyCuK',
+                                    password='m6YJ0faBdk',
+                                    database='riPOnUyCuK',
+                                    cursorclass=pymysql.cursors.DictCursor)
+        cur = connection.cursor()
+    return cur
 #Connection sqlite3
 # conn = sqlite3.connect('hsl_db.sqlite')
 # cur = conn.cursor()
@@ -21,26 +33,26 @@ connection = pymysql.connect(host='remotemysql.com',
 class MessageHealth(Resource):
 
     def get(self):
-        cur = connection.cursor()
+        cur = get_connection(connection)
         return {'message': 'health'}
 
 class CovidExams(Resource):
     def get(self):
-        cur = connection.cursor()
+        cur = get_connection(connection)
         cur.execute("SELECT `DE_EXAME` as nome_exame, COUNT(`DE_EXAME`) AS quantidade FROM   EXAMES GROUP  BY DE_EXAME ORDER  BY Count(*) DESC")
         exames = cur.fetchall()
         return make_response({'exames':exames})
  
 class ClinicaDesistencia(Resource):
     def get(self):
-        cur = connection.cursor()
+        cur = get_connection(connection)
         cur.execute("SELECT ATENDIMENTO.DE_CLINICA AS clinica, COUNT(DESFECHO.DE_DESFECHO) AS desistencias FROM DESFECHO, ATENDIMENTO WHERE DESFECHO.DE_DESFECHO like 'Desistência do atendimento' AND ATENDIMENTO.ID_DESFECHO = DESFECHO.ID_DESFECHO GROUP BY DE_CLINICA ORDER BY COUNT(DESFECHO.DE_DESFECHO) DESC")
         clinicas = cur.fetchall()
         return make_response({'clinicas':clinicas})
         
 class ClinicaAtendimentos(Resource):
     def get(self):
-        cur = connection.cursor()
+        cur = get_connection(connection)
         cur.execute("SELECT DE_CLINICA AS clinica, COUNT(DE_CLINICA) AS atendimentos FROM ATENDIMENTO GROUP BY DE_CLINICA ORDER BY ATENDIMENTOS DESC")
         clinicas = cur.fetchall()
         return make_response({'clinicas':clinicas})
